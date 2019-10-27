c324648ded523e907f2351508cc01670f24a2ad7
Ali Dogan <alidogan.just.for.commits@gmail.com>
2019-10-27 22:16:28 +0100

    Initial commit

--- src/main/java/start/Application.java
+++ src/main/java/start/Application.java
@@ -3,25 +3,26 @@ package start;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
-
+@RestController
 public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hi Axl";
+        return "Hi user";
     }
 
     @RequestMapping("/info")
     public String info() {
-        return "Here is some information";
+        return "Basic information:";
     }
 
 
     public static void main(String[] args) {
-        System.out.println("Application started");
+        System.out.println("Application main called.");
         SpringApplication.run(Application.class, args);
     }
 }
